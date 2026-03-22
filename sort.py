dabc5bc97fd6b41461bb3d9102e2f53296543c69
darlingmota <darlingmotta@gmail.com>
2026-03-22 14:23:18 +0000

    merge sort

--- sort.py
+++ sort.py
@@ -46,3 +46,32 @@ def quick_sort(arr, key):
                 return i + 1
             _quick_sort(arr, 0, len(arr) - 1)
 
+def merge_sort(arr, key):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left, key)
+        merge_sort(right, key)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if get_key(left[i], key) < get_key(right[j], key):
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1 
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
