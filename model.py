8ded33ab726760183a941089cd071256b8f2e3ad
darlingmota <darlingmotta@gmail.com>
2026-03-22 13:23:16 +0000

    Import csv

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-print("hello")
+import csv
+
+class Vehicle:
+    def __init__(self, vehicle_id, brand, model, year, mileage, engine_size, price):
+        self.vehicle_id = int(vehicle_id)
+        self.brand = brand
+        self.model = model
+        self.year = int(year)
+        self.mileage = int(mileage)
+        self.engine_size = float(engine_size)
+        self.price = float(price)
